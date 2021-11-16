3d6a559da543589901b82be0c4f8a66c0f9b59b7
prvnlhr <praveenlohar04@gmail.com>
2021-11-16 15:26:56 +0530

    Added notes to DFS traversal Graph

--- Data Structure/450 DSA Questions/Graph/1. Create a Graph and print it.py
+++ Data Structure/450 DSA Questions/Graph/1. Create a Graph and print it.py
@@ -1,5 +1,4 @@
 class Graph:
-
     def __init__(self, nVertices):
         self.nVertices = nVertices
         self.adjMatrix = [[0 for i in range(nVertices)] for j in range(nVertices)]
@@ -12,7 +11,7 @@ class Graph:
         for i in self.adjMatrix:
             print(i)
 
-
+# _______________________________________________________________
 edgeVertices = [int(i) for i in input().strip().split()]
 V = edgeVertices[0]
 E = edgeVertices[1]
@@ -24,3 +23,14 @@ for i in range(V):
     print(v1, v2)
     g.addEdge(v1, v2)
 g.printGraph()
+# Ex__1
+# V = 4
+# E = 3
+# 0 2
+# 2 1
+# 1 3
+#    0  1  2  3
+# 0  0  0  1  0
+# 1  0  0  1  1
+# 2  1  1  0  0
+# 3  0  1  0  0
