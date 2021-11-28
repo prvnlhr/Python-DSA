1eb9cb8123ae2a3128ddccf933039bda9b05f8ae
prvnlhr <praveenlohar04@gmail.com>
2021-11-28 20:42:52 +0530

    added comments for steps

--- Data Structure/450 DSA Questions/Graph/7.3.0. Dijkstra's Algorithm(adjList ).py
+++ Data Structure/450 DSA Questions/Graph/7.3.0. Dijkstra's Algorithm(adjList ).py
@@ -17,37 +17,46 @@ from heapq import heappush, heapify, heappop
 # print(a)
 
 def dijkstraHelper(adjList, distArray, src):
+    # source to source distance is 0, so mark 0 at distArray[0] = 0
     distArray[src] = 0
     min_heap = []
+    # now push initial src node to minHeap --> heappush(min_heap,[dist,node])
     heappush(min_heap, (0, src))
+
+    # STEP:
+    # 1. pop node from heap
+    # 2. explore all adjNode of popped node
+    # 3. calculate distance of adjNodes which is equal to , popped node weight + adjNode weight
+    # 4. if this calculated distance is smaller then distance array distance for that adjNode, update it in distArray
+    # 5. push this min_node in heap
     while len(min_heap) > 0:
+        # 1.__pop node from top of min_heap
         parent_node = heappop(min_heap)
-        print('parent_node -->', parent_node)
+        # 2.__explore all adjNodes of parent popped node
         for adjNode in adjList[parent_node[1]]:
+            # 3.__calculate the distance of adjNode from parent node,
             parent_node_weight = parent_node[0]
             adjNode_weight = adjNode[1]
             dist_of_adjNode = parent_node_weight + adjNode_weight
             curr_distArray_adjNode_dist = distArray[adjNode[0]]
-            print('parentNode :', parent_node)
-            print('adjNode :', adjNode)
-            print('adjNode_weight:', adjNode_weight, 'parent_node_weight', parent_node_weight)
-            print('curr_distArray_adjNode_dist : ', curr_distArray_adjNode_dist, 'dist_of_adjNode', dist_of_adjNode)
+            # 4.__compare distance with distArray,if smaller update in distArray,
             if dist_of_adjNode < curr_distArray_adjNode_dist:
+                # update in distArray
                 distArray[adjNode[0]] = dist_of_adjNode
+                # 5.__if smaller also push in min_heap
                 heappush(min_heap, (dist_of_adjNode, adjNode[0]))
+                # heapify so that min weight node is always top of min_heap
                 heapify(min_heap)
-                print(min_heap)
-                print(distArray)
-    print(distArray)
-    f = 0
+
+    # printing distArray
+    x = 0
     for ele in distArray:
-        print(f, " ", ele)
-        f = f + 1
+        print(x, " ", ele)
+        x = x + 1
 
 
 def dijkstra(adjList, nVertices, src):
     distArray = [sys.maxsize for _ in range(nVertices)]
-
     dijkstraHelper(adjList, distArray, src)
 
 
