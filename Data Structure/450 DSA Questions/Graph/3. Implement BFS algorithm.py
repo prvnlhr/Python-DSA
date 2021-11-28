c20aeb66537006c23f053ea3cf1cd1cc292d2646
prvnlhr <praveenlohar04@gmail.com>
2021-11-28 20:05:05 +0530

    added time complexity

--- Data Structure/450 DSA Questions/Graph/3. Implement BFS algorithm.py
+++ Data Structure/450 DSA Questions/Graph/3. Implement BFS algorithm.py
@@ -1,3 +1,15 @@
+import queue
+
+
+# Time Complexity__
+# If the graph is represented as adjacency list:: O(V) + O(E) = O(V + E)
+# If the graph is represented as an adjacency matrix (a V x V array):  O(V * V) = O(V2).
+
+# Space Complexity__
+# Since we are maintaining a priority queue (FIFO architecture)
+# to keep track of the visited nodes, in worst case,
+# the queue could take up to the size of the nodes(or vertices) in the graph.
+# Hence, the space complexity is O(V).
 class Graph:
 
     def __init__(self, nVertics):
@@ -8,14 +20,21 @@ class Graph:
         self.adjMatrix[v1][v2] = 1
         self.adjMatrix[v2][v1] = 1
 
-    def bfs_helper(self, visited):
-        pass
+    def bfs_helper(self, visited, nVertices, sv):
+        q = queue.Queue()
+        q.put(sv)
+        visited[sv] = True
+        while q.empty() is False:
+            vertex = q.get()
+            print(vertex, end='')
+            for i in range(nVertices):
+                if (self.adjMatrix[vertex][i] > 0 and visited[i] == False):
+                    q.put(i)
+                    visited[i] = True
 
-
-
-    def Bfs(self):
+    def Bfs(self, V):
         visited = [False for i in range(self.nVertices)]
-        ans = self.bfs_helper(visited)
+        ans = self.bfs_helper(visited, V, 0)
 
 
 edgeVertices = [int(i) for i in input().strip().split()]
@@ -29,4 +48,4 @@ for i in range(E):
     v2 = int(arr[1])
     g.addEdge(v1, v2)
 
-g.Bfs()
+g.Bfs(V)
