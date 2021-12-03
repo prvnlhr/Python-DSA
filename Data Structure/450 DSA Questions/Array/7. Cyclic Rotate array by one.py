cb73a1b379203944cb594f5e87b7235d32b41b45
prvnlhr <praveenlohar04@gmail.com>
2021-12-03 17:52:36 +0530

    Added improved solution

--- Data Structure/450 DSA Questions/Array/7. Cyclic Rotate array by one.py
+++ Data Structure/450 DSA Questions/Array/7. Cyclic Rotate array by one.py
@@ -1,7 +1,7 @@
 # Input:  arr[] = {1, 2, 3, 4, 5}
 # Output: arr[] = {5, 1, 2, 3, 4}
 
-
+# SOL 1__.
 def cyclicRotateArray(arr):
     # Storing last element
     x = arr[len(arr) - 1]
@@ -12,6 +12,17 @@ def cyclicRotateArray(arr):
     # assigning first element equal to x
     arr[0] = x
     return arr
+
+
+# SOL 2__.
+def cyclicRotateArray(arr):
+    i = 0
+    j = len(arr) - 1
+    while i < len(arr):
+        # swap
+        arr[i], arr[j] = arr[j], arr[i]
+        i = i + 1
+    print(arr)
 
 
 arr = [int(i) for i in input().strip().split()]
