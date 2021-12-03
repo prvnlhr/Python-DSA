e7d166d5fd66aa91ef3cef25bef6efc284ac1fb4
prvnlhr <praveenlohar04@gmail.com>
2021-12-03 17:51:41 +0530

    added comments

--- Data Structure/450 DSA Questions/Array/6. Find Union and Intersection of two sorted array.py
+++ Data Structure/450 DSA Questions/Array/6. Find Union and Intersection of two sorted array.py
@@ -1,3 +1,14 @@
+# Ex__1.
+# Input : arr1[] = {1, 3, 4, 5, 7}
+#         arr2[] = {2, 3, 5, 6}
+# Output : Union : {1, 2, 3, 4, 5, 6, 7}
+#          Intersection : {3, 5}
+#
+# Ex__2.
+# Input : arr1[] = {2, 5, 6}
+#         arr2[] = {4, 6, 8, 10}
+# Output : Union : {2, 4, 5, 6, 8, 10}
+#          Intersection : {6}
 def intersection(arr1, arr2):
     i = 0
     j = 0
