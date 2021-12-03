88c24389251512dcefd7e2c4b8632370e007551a
prvnlhr <praveenlohar04@gmail.com>
2021-12-03 17:46:48 +0530

    added time complexity

--- Data Structure/450 DSA Questions/Array/1. Reverse the array.py
+++ Data Structure/450 DSA Questions/Array/1. Reverse the array.py
@@ -1,3 +1,6 @@
+# Time Complexity O(1/2 * N) => O(N) because , 1/2 is constant
+
+
 def reverseArray(arr):
     i = 0
     j = len(arr) - 1
@@ -5,11 +8,9 @@ def reverseArray(arr):
         arr[i], arr[j] = arr[j], arr[i]
         i = i + 1
         j = j - 1
-
     return arr
 
 
 arr = [int(i) for i in input().strip().split()]
-
 ans = reverseArray(arr)
 print(ans)
