68b5d0b832ddbf216bccace5c1f38f4bcc09d963
prvnlhr <praveenlohar04@gmail.com>
2021-12-03 17:50:13 +0530

    added improved solution

--- Data Structure/450 DSA Questions/Array/3. Find kth max and min in  array.py
+++ Data Structure/450 DSA Questions/Array/3. Find kth max and min in  array.py
@@ -1,9 +1,48 @@
-def findKthMaxMin(arr):
-    pass
+import heapq
+import sys
+
+
+# Naive easy solution would be to solve using merge sort.
+# Merge sort would take O(N logN) time and we can find kth smallest and kth largest from sorted array which will
+# take O(1) time. So overall time complexity would be O(N logN) ,neglecting constant time.
+def findKthMaxMin(arr, k):
+    arr = sorted(arr)
+    print(arr)
+    n = len(arr)
+    kthSmallest = arr[k - 1]
+    kthLargest = arr[n - k]
+    print(kthSmallest, kthLargest)
+
+
+# Optimize solution using Heaps:
+# Both findKthMin ,findKthMax are optimize using heap
+# Time Complexity : N log K which it better then O(N log N)
+def findKthMin(arr, k):
+    maxHeap = []
+    for i in range(0, k):
+        maxHeap.append(arr[i])
+    heapq._heapify_max(maxHeap)
+    for i in range(k, len(arr)):
+        if arr[i] < maxHeap[0]:
+            heapq._heapreplace_max(maxHeap, arr[i])
+    print(maxHeap[0])
+    return maxHeap[0]
+
+
+def findKthMax(arr, k):
+    minHeap = []
+    for i in range(0, k):
+        minHeap.append(arr[i])
+    heapq.heapify(minHeap)
+    # print(minHeap)
+    for i in range(k, len(arr)):
+        # print(minHeap)
+        if minHeap[0] < arr[i]:
+            heapq.heapreplace(minHeap, arr[i])
+    print(minHeap[0])
+    return minHeap[0]
 
 
 arr = [int(i) for i in input().strip().split()]
-
-ans = findKthMaxMin(arr)
 k = int(input())
-print(ans)
+ans = findKthMax(arr, k)
