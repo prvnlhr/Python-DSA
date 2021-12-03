8a8c8f86905a3382103496575bbd896662b0478e
prvnlhr <praveenlohar04@gmail.com>
2021-12-03 17:51:06 +0530

    Added comments

--- Data Structure/450 DSA Questions/Array/5. Move all negative elements to one side of array.py
+++ Data Structure/450 DSA Questions/Array/5. Move all negative elements to one side of array.py
@@ -1,3 +1,9 @@
+# Input: -12, 11, -13, -5, 6, -7, 5, -3, -6
+# Output: -12 -6  -13  -5  -3 -7  5   6  11
+
+# IN this solution order of element is not maintained,just negatives are brought to one side
+# another solution exists which maintains the array order,which is hard solution.
+# Two pointer approach , T:O(N) , S:O(1)
 def movePositiveToEnd(arr):
     i = 0
     j = len(arr) - 1
