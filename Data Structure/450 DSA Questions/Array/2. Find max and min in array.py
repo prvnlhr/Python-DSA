6d0398615a80d4ea38a66095625c172486788105
prvnlhr <praveenlohar04@gmail.com>
2021-12-03 17:49:36 +0530

    added improved solution

--- Data Structure/450 DSA Questions/Array/2. Find max and min in array.py
+++ Data Structure/450 DSA Questions/Array/2. Find max and min in array.py
@@ -1,20 +1,19 @@
 import sys
 
 
+# Time Complexity O(1/2 * N) => O(N) because , 1/2 is constant
+
+
 def findMaxMin(arr):
     min = sys.maxsize
     max = ~sys.maxsize
     print(min, max)
-
     for i in range(len(arr)):
         if arr[i] > max:
             max = arr[i]
         if arr[i] < min:
             min = arr[i]
     return min, max
-
-
 arr = [int(i) for i in input().strip().split()]
-
 ans = findMaxMin(arr)
 print(ans)
