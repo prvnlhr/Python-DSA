07bf30aa2223475566e90d74f7e4aca5d26f8c54
prvnlhr <praveenlohar04@gmail.com>
2021-12-13 20:19:19 +0530

    Update code

--- Data Structure/450 DSA Questions/Dynamic Programming/2. ByteLandian.py
+++ Data Structure/450 DSA Questions/Dynamic Programming/2. ByteLandian.py
@@ -10,7 +10,7 @@
 # Input --> Output:
 # 12 --> 13
 # 2 --> 2
-# You can change 12 into 6, 4 and 3, and then change these into 6+4+3=13.
+# You can change 12 into 6, 4 and 3, and then change these into 6 + 4 + 3 = 13.
 # If you try changing the coin 2 into 3 smaller coins, you will get 1, 0 and 0, and later you can get no more than 1 out of them.
 # It is better just to change the 2 coin directly into 2.
 
@@ -28,8 +28,10 @@ def bytelandianRec(n):
 
 def bytelandianMemo(n):
     dp = [-1] * n + 1
+
     if n == 0 or n == 1:
         return n
+
     if (dp[n] == -1):
         ans1 = bytelandianMemo(n // 2)
         ans2 = bytelandianMemo(n // 3)
