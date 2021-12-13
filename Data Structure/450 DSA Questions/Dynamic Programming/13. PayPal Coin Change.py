7940860fda5c61829fc6e10ef1c25f481b10383d
prvnlhr <praveenlohar04@gmail.com>
2021-12-13 20:24:54 +0530

    Added Steps and explanation

--- Data Structure/450 DSA Questions/Dynamic Programming/13. PayPal Coin Change.py
+++ Data Structure/450 DSA Questions/Dynamic Programming/13. PayPal Coin Change.py
@@ -3,7 +3,7 @@ import math, sys
 # Given a value V, if we want to make change for V cents,
 # and we have infinite supply of each of C = { C1, C2, .. , Cm} valued coins,
 # what is the minimum number of coins to make the change?
-
+#
 # Examples:
 #
 # Input: coins[] = {25, 10, 5}, amount = 30
