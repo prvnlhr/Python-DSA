9c73cb6d5bcfd528a958726ee94097f9ca55dabf
prvnlhr <praveenlohar04@gmail.com>
2021-12-13 20:24:36 +0530

    Added Steps and explanation

--- Data Structure/450 DSA Questions/Array/13. Merge of Intervals.py
+++ Data Structure/450 DSA Questions/Array/13. Merge of Intervals.py
@@ -1,21 +1,45 @@
 # arr = [[6, 8], [1, 9], [2, 4], [4, 7]]
 # output = [1, 9]
 
+
+# Time complexity : O(NlogN)
+# Space complexity : O(NlogN) or O(N)
+
+# STEPS:
+# 1. Sort the array on basis of first element(start) of pair [[start,end],[start,end]]
+# 2. Initialize stack with first element of array
+# 3. for every element of input arr, there may be two conditions,
+# ____if not overlapping --> since the array is sorted on basis of their start value, so to check
+#                            overlapping we can compare curr element start top stack_top start,
+#                            if stack_top start is smaller then no overlapping,hence append curr top stack
+# ____if overlapping --> we do not check anything just change the stack top elements end with max(stack_top_end ,curr_end)
+
+
 def mergeInterval(arr):
     # Sorting based on the increasing order
     # of the start intervals
+
     arr.sort(key=lambda x: x[0])
+    stack = []
+    # initialize stack with arr first element,
+    stack.append(arr[0])
 
-    merged = []
+    for curr in arr:
+        # for every element in arr,
+        stack_top = stack[-1]
+        stack_top_end = stack_top[1]
+        curr_start = curr[0]
 
-    for interval in arr:
-        if not merged or merged[-1][1] < interval[0]:
-            merged.append(interval)
+        # if this condition satisfies then there is no overlapping
+        if stack_top_end < curr_start:
+            stack.append(curr)
+        # else there is overlapping just change the stack top element and move for next iteration
         else:
-            merged[-1][1] = max(merged[-1][1], interval[1])
-    return merged
+            stack_top[1] = max(stack_top[1], curr[1])
+    return stack
 
 
 arr = [[6, 8], [1, 9], [2, 4], [4, 7]]
+arr = [[1, 3], [2, 6], [8, 10], [15, 18]]
 ans = mergeInterval(arr)
 print(ans)
