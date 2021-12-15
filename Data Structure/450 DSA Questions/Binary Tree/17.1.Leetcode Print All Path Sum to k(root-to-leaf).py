9aa65df15483e5aa141b315fd379170e23e2de4e
prvnlhr <praveenlohar04@gmail.com>
2021-12-15 16:02:22 +0530

    Rectified

--- Data Structure/450 DSA Questions/Binary Tree/17.1.Leetcode Print All Path Sum to k(root-to-leaf).py
+++ Data Structure/450 DSA Questions/Binary Tree/17.1.Leetcode Print All Path Sum to k(root-to-leaf).py
@@ -1,7 +1,21 @@
 import BinaryTreeInputPrint
 
 
-def pathSumHelper(root, sum, path, result):
+def pathSumHelperCN(root, x, path, currSum):
+    if root is None:
+        return None
+
+    if root.left is None and root.right is None:
+        currSum = currSum + root.data
+
+        if currSum == x:
+            print(str(path + str(root.data) + ' ').lstrip())
+        return
+    pathSumHelperCN(root.left, x, str(path + str(root.data) + ' '), currSum + root.data)
+    pathSumHelperCN(root.right, x, str(path + str(root.data) + ' '), currSum + root.data)
+
+
+def pathSumHelperLeetcode(root, sum, path, result):
     if root is None:
         return None
 
@@ -9,15 +23,17 @@ def pathSumHelper(root, sum, path, result):
         result.append([*path, root.data])
         print(result)
         return None
+
     path.append(root.data)
-    pathSumHelper(root.left, sum - root.data, path, result)
-    pathSumHelper(root.right, sum - root.data, path, result)
+    pathSumHelperLeetcode(root.left, sum - root.data, path, result)
+    pathSumHelperLeetcode(root.right, sum - root.data, path, result)
     path.pop()
 
 
 def pathSum(root, sum):
     result = []
-    pathSumHelper(root, sum, [], result)
+    # pathSumHelperLeetcode(root, sum, [], result)
+    pathSumHelperCN(root, sum, ' ', 0)
     return result
 
 
