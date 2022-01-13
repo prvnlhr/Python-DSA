8a9bd74848f3f66ffed09b07607dce8ab75b4130
prvnlhr <praveenlohar04@gmail.com>
2022-01-13 18:55:22 +0530

    code done

--- Data Structure/450 DSA Questions/Stacks and Queues/5.Reverse a string Using Stack.py
+++ Data Structure/450 DSA Questions/Stacks and Queues/5.Reverse a string Using Stack.py
@@ -2,9 +2,12 @@ def reverseAStringUsingStack(s):
     stack = []
     curr = 0
 
+    # One by one put a characters of string to stack
     while curr < len(s):
         stack.append(s[curr])
         curr = curr + 1
+
+    # one by one pop elements form stack and make a string
     ans = ''
     while stack:
         ele = stack[-1]
