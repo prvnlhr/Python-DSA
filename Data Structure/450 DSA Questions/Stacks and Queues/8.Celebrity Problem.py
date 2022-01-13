b1e05f6bc3f3c5dc9271ec024acaf28e3f9f3193
prvnlhr <praveenlohar04@gmail.com>
2022-01-13 18:56:09 +0530

    code done

--- Data Structure/450 DSA Questions/Stacks and Queues/8.Celebrity Problem.py
+++ Data Structure/450 DSA Questions/Stacks and Queues/8.Celebrity Problem.py
@@ -1,7 +1,7 @@
 # Is Based on The Approach of Graph
 # This Problem has many solution based on Graph, Two-pointer,Stack
 # Stack Sol is O(N) and O(N)
-# Two Pointer sol are of Two types: 1) O(n^2) 2) O(n) and O(1)
+# Two Pointer sol are of Two types: 1) O(n ^ 2) 2) O(n) and O(1)
 #
 #
 
@@ -9,17 +9,18 @@
 def findCelebrity(n, mat):
     start = 0
     end = n - 1
-
+    # this loop find a potential candidate which can be celebrity
     while start < end:
         if mat[start][end] == 1:
             start = start + 1
         else:
             end = end - 1
-
+    candidate = start
+    # this loop finally , check if candidate is celebrity or not
     for i in range(n):
-        if i != start and (mat[start][i] == 1 or mat[i][start] == 0):
+        if i != candidate and (mat[candidate][i] == 1 or mat[i][candidate] == 0):
             return -1
-    return start
+    return candidate
 
 
 n = int(input())
